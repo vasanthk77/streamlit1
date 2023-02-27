c37ec24dfbdf2e5a2d50867a94b79f9113c79d9a
Vasanth K <116260385+vasanthk77@users.noreply.github.com>
2023-02-27 10:57:40 +0530

    Update app.py

--- proj_2/app.py
+++ proj_2/app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.header("Vasanth K")
 st.subheader("Data science intern at :red[Innomatics Research Labs]")
-st.subheader("student at :blue[presidency university] :Green[computer science] 3rd Year")
-st.subheader("Aspiring to be an Data Scientist")
+st.subheader("student at :blue[presidency university] :red[computer science] 3rd Year")
+st.subheader("Aspiring to be an :red[Data Scientist]")
